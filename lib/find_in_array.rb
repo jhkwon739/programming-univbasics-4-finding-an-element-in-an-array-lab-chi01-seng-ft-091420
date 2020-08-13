8c7159d40cbfc07c3edd6bc99f1cd4839ca5ad4e
Jasmine Kwon <jhkwon1472@gmail.com>
2020-08-13 03:02:21 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,6 +2,6 @@ def find_element_index(array, value_to_find)
   # Add your solution here
   #solution using built in method => array.index(value_to_find)
   index = 0
-  array.length.times { |i| if array[i] == value_to_find index = i else index = nil  end}
-  index
-
+  while index < array.length do
+    if array[inde
+end
